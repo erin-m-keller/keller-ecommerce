5d6ebc3dfaef699bd4655a3b438bd50fa6cb880e
Erin Keller <erinkeller@Erins-MacBook-Pro-2.local>
2023-04-27 10:47:55 -0400

    Added columns to Category model

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,19 @@
-const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection.js');
+const { Model, DataTypes } = require('sequelize'),
+      sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    category_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
