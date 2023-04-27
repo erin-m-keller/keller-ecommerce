f938142c932fc1772ceaad380d8b8a82577d2b1d
Erin Keller <erinkeller@Erins-MacBook-Pro-2.local>
2023-04-27 10:57:14 -0400

    Working on product model

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    category_id: { 
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'category_id',
+        unique: true
+      }
+    },
+    product_tag: { // TODO: in tag model
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
