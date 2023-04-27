cfac9b59fc9fec943145ed37bf5e89da1faebba9
Erin Keller <erinkeller@Erins-MacBook-Pro-2.local>
2023-04-27 11:52:12 -0400

    Added allowNull: false

--- models/Product.js
+++ models/Product.js
@@ -28,6 +28,7 @@ Product.init(
     },
     category_id: { 
       type: DataTypes.INTEGER,
+      allowNull: false,
       references: {
         model: 'category',
         key: 'category_id',
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -13,9 +13,11 @@ ProductTag.init(
     },
     product_id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
     },
     tag_id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       references: {
         model: 'tag',
         key: 'id',
